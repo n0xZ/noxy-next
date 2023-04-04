c4b9b89382445f2a491f471b2bc74f387b26e144
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-04-04 19:18:33 -0300

    chore: setup initial page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,24 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { redis } from '~/lib/upstash.server'
 
-const inter = Inter({ subsets: ['latin'] })
+export const metadata = {
+	title: 'Welcome to noxy-next!',
+}
+const InitCount = async () => {
+	const result = await redis.incr('count-value')
+	return result
+}
+export default async function Home() {
+	const count = await InitCount()
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className="container flex flex-col items-center justify-between h-full max-w-xl mx-auto space-y-3">
+			<section className='flex flex-col items-center justify-center min-h-screen p-1 space-y-4'>
+				<h2 className="text-4xl text-center">Next 13 + Upstash = ❤</h2>
+				<p className='text-lg xl:text-xl'>Cantidad de veces vistas: <span className='text-blue-500'>{count}</span></p>
+			</section>
+			<footer className="p-5">
+				Powered by <span className='text-blue-500'>NextJS 💙</span>
+			</footer>
+		</main>
+	)
 }
